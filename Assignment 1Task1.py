eb558270bdaabe0dc9032371de1a6763dd72448f
nikhilsindhi <nikkhilsindhi@gmail.com>
2025-05-20 23:53:08 +0530

    Uploaded Assignment Task 1 and Task 2

---
+++ Assignment 1Task1.py
@@ -0,0 +1,11 @@
+#Task1
+a=float(input('Enter rhe first number:'))
+b=float(input('Enter rhe second number:'))
+add=a+b
+sub=a-b
+mult=a*b
+div=a/b
+print ('Addition: ', add)
+print ('Subtraction: ', sub)
+print ('Multiplication', mult)
+print ('Division', div)
---
+++ Assignment 1Task2.py
@@ -0,0 +1,5 @@
+#Task 2
+a=input('Enter First Nam')
+b=input('Enter Last Name')
+concat=a+ ' ' +b
+print('Hello,',concat,'! Welcome to the Python Program' )
